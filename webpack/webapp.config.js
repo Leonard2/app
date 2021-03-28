1997a602708ab9218e325f9af760e820b17194e1
Lénard <hobbax3@gmail.com>
2021-03-28 23:26:15 +0200

    add building options for a simple web app

---
+++ webpack/webapp.config.js
@@ -0,0 +1,39 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const Path = require('path');
+const BaseConfig = require('./base.config');
+
+module.exports = Object.assign(BaseConfig, {
+  entry: {
+    app: Path.resolve(__dirname, '../res/webapp/main.ts')//
+  },
+  output: {
+    path: Path.resolve(__dirname, '../build/webapp/assets')
+  },
+ plugins: BaseConfig.plugins.concat([
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: Path.resolve(__dirname, '../../res/strings'),
+          to: '../_locales/[name]/messages.json',
+          toType: 'template',
+          transform(buffer) {
+            const i18n = JSON.parse(buffer.toString());//
+            const messages = i18n;
+            return JSON.stringify(messages, null, 2);
+          }
+        },
+        {
+          from: Path.resolve(__dirname, '../../res/webext/images')//
+        },
+        {
+          from: Path.resolve(__dirname, '../res/webext/manifest.json'),//
+          to: '../manifest.json'
+        },
+		{
+          from: Path.resolve(__dirname, '../res/webapp'),
+          to: '../'
+        }
+      ]
+    }),
+  ])
+});

c89a9294e19628629eb54ae35006d02d3297db6b
Lénard <hobbax3@gmail.com>
2021-03-30 01:16:31 +0200

    yes

--- src/modules/webapp/webapp-app/webapp-app.component.ts
+++ src/modules/webapp/webapp-app/webapp-app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from 'angular-ts-decorators';
 import autobind from 'autobind-decorator';
 import AppMainComponent from '../../app/app-main/app-main.component';
+import WebAppPlatformService from '../webapp-shared/webapp-platform/webapp-platform.service';
 
 @autobind
 @Component({
@@ -10,6 +11,8 @@ import AppMainComponent from '../../app/app-main/app-main.component';
   template: require('../../app/app-main/app-main.component.html')
 })
 export default class WebAppAppComponent extends AppMainComponent implements OnInit {
+  platformSvc: WebAppPlatformService;
+
   static $inject = [
     '$q',
     '$scope',
@@ -27,6 +30,6 @@ export default class WebAppAppComponent extends AppMainComponent implements OnIn
   ];
 
   ngOnInit(): ng.IPromise<void> {
-    return super.ngOnInit();
+    return this.platformSvc.initI18n().then(() => super.ngOnInit());
   }
 }
